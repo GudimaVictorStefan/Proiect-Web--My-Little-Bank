44cc1034c3865d5295ad3820cda4ef7f16622193
GudimaVictorStefan <122046840+GudimaVictorStefan@users.noreply.github.com>
2023-01-15 18:32:38 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const account1 = {
-  owner: 'Victor Gudima',
+  owner: 'Gudima Victor',
   movements: [200, 455.23, -306.5, 25000, -642.21, -133.9, 79.97, 1300],
   interestRate: 1.2,
   pin: 1234,
